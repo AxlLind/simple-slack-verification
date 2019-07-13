b3dcdbf2b7cb3d271bd3024ff85d696405555e07
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2019-07-13 21:32:19 +0200

    Switch to old syntax for better support

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-const crypto = require("crypto");
-const qs = require("qs");
+var crypto = require('crypto');
+var qs = require('qs');
 
-const DEFAULT_RESPONSE = {
-  code: "unauthorized",
-  response_type: "ephemeral",
-  text: "Not an authorized Slack request."
+var DEFAULT_RESPONSE = {
+  code: 'unauthorized',
+  response_type: 'ephemeral',
+  text: 'Not an authorized Slack request.'
 };
 
-const noopMiddleware = (_req, _res, next) => next();
+function noopMiddleware(_req, _res, next) {
+  next();
+}
 
-const slackAuth = ({
-  secret = process.env.SLACK_SIGNING_SECRET,
-  unauthorizedResponse = DEFAULT_RESPONSE,
-  maxSecondsOld = 300,
-  disabled
-}) => {
-  if (disabled) {
+function slackAuth(options) {
+  if (options.disabled) {
     return noopMiddleware;
   }
-  return (req, res, next) => {
-    const timestamp = req.headers["x-slack-request-timestamp"];
-    const givenSignature = req.headers["x-slack-signature"];
+  var secret = options.secret || process.env.SLACK_SIGNING_SECRET;
+  var unauthorizedResponse = options.unauthorizedResponse || DEFAULT_RESPONSE;
+  var maxSecondsOld = options.maxSecondsOld || 300;
+
+  return function slackAuth(req, res, next) {
+    var timestamp = req.headers['x-slack-request-timestamp'];
+    var givenSignature = req.headers['x-slack-signature'];
 
     if (
       !timestamp ||
@@ -30,19 +31,20 @@ const slackAuth = ({
       return res.send(401).send(unauthorizedResponse);
     }
 
-    const rawBody = qs.stringify(req.body, { format: "RFC1738" });
-    const signature = crypto
-      .createHmac("sha256", secret)
-      .update(`v0:${timestamp}:${rawBody}`)
-      .digest("hex");
+    var rawBody = qs.stringify(req.body, { format: 'RFC1738' });
+    var signature = crypto
+      .createHmac('sha256', secret)
+      .update('v0:' + timestamp + ':' + rawBody)
+      .digest('hex');
     if (
       !crypto.timingSafeEqual(
-        Buffer.from(`v0=${signature}`, "utf8"),
-        Buffer.from(givenSignature, "utf8")
+        Buffer.from('v0=' + signature, 'utf8'),
+        Buffer.from(givenSignature, 'utf8')
       )
     ) {
       return res.send(401).send(unauthorizedResponse);
     }
+
     next();
   };
 };
